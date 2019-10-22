ba9cbd41ccafc89ba3f34248e53dcdfe4ec122c4
Merisho <merishot@gmail.com>
2019-10-22 22:24:26 +0300

    Corrected solution

--- rayon2013_b.cpp
+++ rayon2013_b.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int talentsCompetition(int n, int k, int f[10][10]);
-int fsum(int xstart, int ystart, int xend, int yend, int xdir, int ydir, int f[10][10]);
-int findMax(int n, int k, int f[10][10], int xstart, int ystart, int xdir, int ydir);
+int diagonalSum(int x, int y, int size, int n, int k, int f[10][10]);
+int sum(int x, int y, int size, int f[10][10]);
 
 int main()
 {
@@ -24,40 +24,55 @@ int main()
 	return 0;
 }
 
-int talentsCompetition(int n, int k, int f[10][10])
-{
-	int max1 = findMax(n, k, f, 0, 0, 1, 1);
-	int max2 = findMax(n, k, f, n - 1, 0, -1, 1);
-	int max3 = findMax(n, k, f, 0, n - 1, 1, -1);
-	int max4 = findMax(n, k, f, n - 1, n - 1, -1, -1);
-
-	int m1 = max1 > max2 ? max1 : max2;
-	int m2 = max3 > max4 ? max3 : max4;
-	return m1 > m2 ? m1 : m2;
-}
-
-int findMax(int n, int k, int f[10][10], int xstart, int ystart, int xdir, int ydir) {
-	int x = xstart;
-	int y = ystart;
-	int max = 0;
-	for (int size = 1; size <= n; size++) {
-		if (fsum(xstart, ystart, x, y, xdir, ydir, f) <= k) {
-			max = size;
+int talentsCompetition(int n, int k, int f[10][10]) {
+	for (int size = n; size >= 2; size--) {
+		for (int x = 0; x <= n - size; x++) {
+			if (diagonalSum(x, 0, size, n, k, f) <= k) {
+				return size;
+			}
 		}
-		x += xdir;
-		y += ydir;
+
+		if (size == n) {
+			continue;
+		}
+
+		int x = n - size;
+		for (int y = 1; y <= n - size; y++) {
+			if (diagonalSum(x, y, size, n, k, f) <= k) {
+				return size;
+			}
+		}
 	}
 
-	return max;
+	return 1;
 }
 
-int fsum(int xstart, int ystart, int xend, int yend, int xdir, int ydir, int f[10][10])
-{
+int diagonalSum(int x, int y, int size, int n, int k, int f[10][10]) {
+	if (x - 1 < 0 || y + 1 > n - size) {
+		return sum(x, y, size, f);
+	}
+
+	int s = sum(x, y, size, f);
+	if (s <= k) {
+		return s;
+	}
+	
+	return diagonalSum(x - 1, y + 1, size, n, k, f);
+}
+
+int sum(int xstart, int ystart, int size, int f[10][10]) {
+	int xend = xstart + size - 1;
+	int yend = ystart + size - 1;
 	int s = 0;
-	for (int y = ystart; y != yend + ydir; y += ydir) {
-		for (int x = xstart; x != xend + xdir; x += xdir) {
+
+	int y = ystart;
+	while (y <= yend) {
+		int x = xstart;
+		while (x <= xend) {
 			s += f[y][x];
+			x++;
 		}
+		y++;
 	}
 
 	return s;
