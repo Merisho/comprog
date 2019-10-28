25953dc2b84d76fcb49f656a3592f6e500e68b76
Merisho <merishot@gmail.com>
2019-10-28 18:31:37 +0200

    C: solution

--- c_mult_table.cpp
+++ c_mult_table.cpp
@@ -3,13 +3,7 @@
 #include <algorithm>
 using namespace std;
 
-int d(int, int);
-
-int D[1000000];
-
 int main() {
-	D[1] = 0;
-
 	long long n;
 	cin >> n;
 
@@ -18,33 +12,7 @@ int main() {
 		k--;
 	}
 
-	if (k == 1) {
-		cout << n - 1;
-	}
-	else {
-		cout << d(k, n / k);
-	}
+	cout << k + n / k - 2;
 
 	return 0;
-}
-
-int d(int x, int y) {
-	if (x == 1 && y == 1) {
-		return D[1];
-	}
-
-	int m = max(x, y);
-	if (D[m] != 0) {
-		return D[m];
-	}
-
-	if (x == 1) {
-		return D[m] = 1 + d(x, y - 1);
-	}
-
-	if (y == 1) {
-		return D[m] = 1 + d(x - 1, y);
-	}
-
-	return D[m] = 1 + min(d(x - 1, y), d(x, y - 1));
 }
