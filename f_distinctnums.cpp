92c394b21aee07889488013b35cc4db0765f6262
Merisho <merishot@gmail.com>
2019-11-01 20:33:06 +0200

    F: apply correct solution (found and understood ready solution)

--- f_distinctnums.cpp
+++ f_distinctnums.cpp
@@ -4,42 +4,33 @@
 #include <algorithm>
 using namespace std;
 
-int sum(vector<int>&, vector<int>&, int);
-int findGreater(vector<int>&, int);
-
-int m[300000];
-
 int main() {
 	int n;
 	cin >> n;
 
-	for (int i = 0; i < n; i++) {
+	vector<int> groups(n);
+	for (int i = 0; i < n; ++i) {
 		int t;
 		cin >> t;
-		m[t]++;
-	}
-
-	vector<int> groups;
-	for (int i = 0; i < 300000; ++i) {
-		if (m[i] > 0) {
-			groups.push_back(m[i]);
-		}
+		groups[t - 1]++;
 	}
 
 	sort(groups.begin(), groups.end());
 
-	vector<int> sums;
-	sums.push_back(groups[0]);
-	for (int i = 1; i < groups.size(); i++) {
-		sums.push_back(sums[i - 1] + groups[i]);
+	vector<int> sums(n + 1);
+	for (int i = 0; i < n; i++) {
+		sums[i + 1] = sums[i] + groups[i];
 	}
 
-	cout << n << endl;
-	int prevTimes = n;
-	for (int k = 2; k <= n; ++k) {
-		int times = prevTimes;
+	int times = n;
+	int lastSmaller = n;
+	for (int k = 1; k <= n; ++k) {
 		while (times > 0) {
-			if (sum(groups, sums, times) >= k * times) {
+			while (lastSmaller > 0 && groups[lastSmaller - 1] >= times) {
+				lastSmaller--;
+			}
+
+			if (times * (n - lastSmaller) + sums[lastSmaller] >= k * times) {
 				break;
 			}
 
@@ -47,42 +38,7 @@ int main() {
 		}
 
 		cout << times << endl;
-		prevTimes = times;
 	}
 
 	return 0;
-}
-
-int sum(vector<int>& groups, vector<int>& sums, int times) {
-	int i = findGreater(groups, times);
-
-	if (i == -1) {
-		return groups.size() * times;
-	}
-
-	return (groups.size() - i - 1) * times + sums[i];
-}
-
-int findGreater(vector<int>& v, int times) {
-	int len = v.size();
-	int start = 0;
-	int end = len - 1;
-	while (start < end) {
-		int m = start + (end - start) / 2;
-		if (v[m] < times && v[m + 1] < times) {
-			start = m + 1;
-		}
-		else if (v[m] >= times && v[m + 1] >= times) {
-			end = m;
-		}
-		else {
-			return m;
-		}
-	}
-
-	if (end == 0) {
-		return -1;
-	}
-
-	return len - 1;
 }
