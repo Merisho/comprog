2283f2ecbe1124695b043b7c067b376a0dfc8d6d
Merisho <merishot@gmail.com>
2019-12-11 09:43:08 +0200

    D: no solution

--- d_knight.cpp
+++ d_knight.cpp
@@ -3,27 +3,8 @@ using namespace std;
 
 constexpr long long MOD = 1e9 + 7;
 
-long long k = -1;
-
-void ways(int x, int y) {
-	if (x - 1 < 0 || y - 1 < 0 || x - 2 < 0 || y - 2 < 0) {
-		return;
-	}
-
-	++k;
-
-	ways(x - 2, y - 1);
-	ways(x - 1, y - 2);
-}
-
 int main() {
-	int x, y;
-	cin >> x >> y;
-	
-	ways(x, y);
-
-	cout << k % MOD;
+	cout << "I will get back to this later";
 
 	return 0;
 }
-	
