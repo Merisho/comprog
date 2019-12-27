f24a5bc7a2893d98fcbbf5c1a5aede6fa4ad688a
Merisho <merishot@gmail.com>
2019-12-27 16:32:19 +0200

    Almost correct solution

--- f_tag_tree.cpp
+++ f_tag_tree.cpp
@@ -3,20 +3,25 @@ using namespace std;
 
 constexpr int INF = 1e9 + 1;
 
+vector<int> adj[100000];
+
+void dfs(int d[100000], int p, int v, int k = 1) {
+	for (int i = 0; i < adj[v].size(); ++i) {
+		int c = adj[v][i];
+		if (c == p) {
+			continue;
+		}
+
+		d[c] = k;
+		dfs(d, v, c, k + 1);
+	}
+}
+
 int main() {
 	int n, u, v;
 	cin >> n >> u >> v;
 	--u;
 	--v;
-
-	int d[n][n];
-	for (int i = 0; i < n; ++i) {
-		for (int j = 0; j < n; ++j) {
-			d[i][j] = INF;
-		}
-
-		d[i][i] = 0;
-	}
 
 	for (int i = 0; i < n - 1; ++i) {
 		int a, b;
@@ -24,64 +29,24 @@ int main() {
 
 		--a;
 		--b;
-		d[a][b] = 1;
-		d[b][a] = 1;
+		adj[a].push_back(b);
+		adj[b].push_back(a);
 	}
 
+	int ud[100000];
+	dfs(ud, u, u);
+
+	int vd[100000];
+	dfs(vd, v, v);
+
+	int ans = 0;
 	for (int i = 0; i < n; ++i) {
-		stack<int> p;
-		p.push(i);
-
-		bool u[n];
-		while (!p.empty()) {
-			int v = p.top();
-			p.pop();
-
-			u[v] = true;
-
-			for (int j = 0; j < n; ++j) {
-				int a = d[v][j];
-				if (a == 1 && !u[j]) {
-					p.push(j);
-					d[i][j] = min(d[i][j], d[i][v] + 1);
-					d[j][i] = d[i][j];
-				}
-			}
+		if (ud[i] < vd[i] && vd[i] > ans) {
+			ans = vd[i] - 1;
 		}
 	}
 
-	int k = 0;
-	while (u != v) {
-		int dd = -INF;
-		for (int i = 0; i < n; ++i) {
-			if (i == u) {
-				continue;
-			}
-
-			if (d[v][i] - d[u][i] > dd) {
-				dd = d[v][i] - d[u][i];
-				u = i;
-
-				if (u == v) {
-					break;
-				}
-
-				for (int j = 0; j < n; ++j) {
-					if (j == v) {
-						continue;
-					}
-
-					if (d[v][j] + d[j][i] == d[v][i]) {
-						v = j;
-						k += d[v][j];
-						break;
-					}
-				}
-			}
-		}
-	}
-
-	cout << k;
+	cout << ans;
 	
 	return 0;
 }
