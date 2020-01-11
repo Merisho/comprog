34002901f250f222504b41cf8c7cf4b3df9aa2a8
Merisho <merishot@gmail.com>
2020-01-11 13:38:48 +0200

    E: solution

--- e_parity.cpp
+++ e_parity.cpp
@@ -3,42 +3,71 @@ using namespace std;
 
 constexpr int INF = 1e9;
 
-int a[200001];
+vector<vector<int>> g;
+vector<int> ans;
 
-int find(int n, int start, int r) {
-	if (a[start] % 2 != r) {
-		return 0;
+void find(vector<int>& targetParity, vector<int>& oppParity) {
+	vector<int> d(targetParity.size() + oppParity.size(), INF);
+	queue<int> q;
+	for (int v : oppParity) {
+		d[v] = 0;
+		q.push(v);
 	}
 
-	int p = start + a[start];
-	int m = start - a[start];
+	while (!q.empty()) {
+		int v = q.front();
+		q.pop();
 
-	if (m > 0 && p <= n) {
-		return 1 + min(find(n, m, r), find(n, p, r));
+		for (int from : g[v]) {
+			if (d[from] == INF) {
+				d[from] = d[v] + 1;
+				q.push(from);
+			}
+		}
 	}
 
-	if (p <= n) {
-		return 1 + find(n, p, r);
+	for (int v : targetParity) {
+		if (d[v] != INF) {
+			ans[v] = d[v];
+		}
 	}
-
-	if (m > 0) {
-		return 1 + find(n, m, r);
-	}
-
-	return INF;
 }
 
 int main() {
 	int n;
 	cin >> n;
 
-	for (int i = 1; i <= n; ++i) {
-		cin >> a[i];
+	g = vector<vector<int>>(n);
+	ans = vector<int>(n, -1);
+
+	vector<int> odd;
+	vector<int> even;
+	for (int i = 0; i < n; ++i) {
+		int a;
+		cin >> a;
+
+		if (a % 2 == 0) {
+			even.push_back(i);
+		} else {
+			odd.push_back(i);
+		}
+
+		int l = i - a;
+		int r = i + a;
+		if (l >= 0) {
+			g[l].push_back(i);
+		}
+
+		if (r < n) {
+			g[r].push_back(i);
+		}
 	}
 
-	for (int i = 1; i <= n; ++i) {
-		int ans = find(n, i, a[i] % 2);
-		cout << (ans >= INF ? -1 : ans) << " ";
+	find(even, odd);
+	find(odd, even);
+
+	for (int i = 0; i < n; ++i) {
+		cout << ans[i] << " ";
 	}
 	
 	return 0;
