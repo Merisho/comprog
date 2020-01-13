25e7e5e19d985483a4c8686707b4a3e573d7daa0
Merisho <merishot@gmail.com>
2020-01-13 21:37:40 +0200

    C: solution

--- c_atcoder.cpp
+++ c_atcoder.cpp
@@ -5,7 +5,8 @@ int main() {
 	int n, m;
 	cin >> n >> m;
 
-	vector<bool> p(n + 1);
+	vector<bool> p(n + 1, false);
+	vector<int> w(n + 1, 0);
 	int wa = 0;
 	int ac = 0;
 	for (int i = 0; i < m; ++i) {
@@ -21,8 +22,9 @@ int main() {
 		if (si == "AC") {
 			p[pi] = true;
 			++ac;
+			wa += w[pi];
 		} else {
-			++wa;
+			++w[pi];
 		}
 	}
 
