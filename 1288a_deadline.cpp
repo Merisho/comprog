f52e6731fc99497ad22bde2153ba035b604bb480
Merisho <merishot@gmail.com>
2020-01-24 09:51:17 +0200

    1288 A: solution

--- 1288a_deadline.cpp
+++ 1288a_deadline.cpp
@@ -1,30 +1,10 @@
-// UNSOLVED
-
 #include <bits/stdc++.h>
 using namespace std;
 
-long long f(long long d, long long x) {
-	int a = d / (x + 1);
+int f(int x, int d) {
+	int ans = x + d / (x + 1);
 	if (d % (x + 1) != 0) {
-		++a;
-	}
-
-	return x + a;
-}
-
-long long find(long long d) {
-	long long l = 2;
-	long long r = d - 1;
-	long long ans = f(d, 1);
-	while (l <= r) {
-		long long m = l + (r - l) / 2;
-		long long res = f(d, m);
-		if (res < f(d, m - 1)) {
-			ans = res;
-			l = m + 1;
-		} else {
-			r = m - 1;
-		}
+		return ans + 1;
 	}
 
 	return ans;
@@ -38,7 +18,15 @@ int main() {
 		long long n, d;
 		cin >> n >> d;
 
-		if (d <= n || find(d) <= n) {
+		bool ans = false;
+		for (int i = 0; i <= sqrt(d); ++i) {
+			if (f(i, d) <= n) {
+				ans = true;
+				break;
+			}
+		}
+
+		if (ans) {
 			cout << "YES";
 		} else {
 			cout << "NO";
