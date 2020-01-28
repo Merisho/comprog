b332bff9a40352f5956b61e1a162ac5b17203060
Merisho <merishot@gmail.com>
2020-01-28 09:01:01 +0200

    1252 A, 1253 A: solutions

---
+++ 1253a_one_lift.cpp
@@ -0,0 +1,51 @@
+// UNSOLVED
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	for (int t = 0; t < T; ++t) {
+		int n;
+		cin >> n;
+
+		vector<int> a(n);
+		for (int& ai : a) {
+			cin >> ai;
+		}
+
+		vector<int> d(n + 2, 0);
+		for (int i = 0; i < n; ++i) {
+			int b;
+			cin >> b;
+			d[i + 1] = b - a[i];
+		}
+
+		int prev = 0;
+		int l = 0;
+		for (int i = 0; i < n + 2; ++i) {
+			int di = d[i];
+			if (di < 0) {
+				l = 3;
+				break;
+			}
+
+			if (di != prev) {
+				++l;
+				prev = di;
+			}
+		}
+
+		if (l < 3) {
+			cout << "YES";
+		} else {
+			cout << "NO";
+		}
+
+		cout << endl;
+	}
+	
+	return 0;
+}
