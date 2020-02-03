376ad05c9ec27049186f7ef3b34d5a69f37e1c36
Merisho <merishot@gmail.com>
2020-02-03 05:01:38 +0200

    A: solution; B: no solution

---
+++ b_array.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int T;
+	cin >> T;
+
+	for (int t = 0; t < T; ++t) {
+		int n;
+		cin >> n;
+
+		bool ans = true;
+		for (int i = 0; i < n; ++i) {
+			int a;
+			cin >> a;
+			
+			
+		}
+
+		if (ans) {
+			cout << "Yes";
+		} else {
+			cout << "No";
+		}
+
+		cout << endl;
+	}
+	
+	return 0;
+}
+	
