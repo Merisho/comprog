337baf22215a0862f962bba02a5fe0a922943de5
Merisho <merishot@gmail.com>
2020-02-21 14:00:41 +0200

    C: solution

--- c_conditioner.cpp
+++ c_conditioner.cpp
@@ -9,29 +9,54 @@ int main() {
 		int n, m;
 		cin >> n >> m;
 
-		int pt = 0;
-		int pl = m;
-		int ph = m;
-		bool ans = true;
+		vector<int[3]> c(n);
 		for (int i = 0; i < n; ++i) {
 			int t, l, h;
 			cin >> t >> l >> h;
 
+			c[i][0] = t;
+			c[i][1] = l;
+			c[i][2] = h;
+		}
+
+		bool ans = true;
+		int tp = 0;
+		int lp = m;
+		int hp = m;
+		for (int i = 0; i < n; ++i) {
+			int t = c[i][0];
+			int l = c[i][1];
+			int h = c[i][2];
+
+			int j = i + 1;
+			while (j < n && t == c[j][0]) {
+				l = max(l, c[j][1]);
+				h = min(h, c[j][2]);
+				if (l > h) {
+					ans = false;
+					break;
+				}
+
+				i = j;
+				++j;
+			}
+
 			if (!ans) {
-				continue;
+				break;
 			}
 
-			int d1 = h - pl;
-			int d2 = l - ph;
-			int dt = t - pt;
+			int d = t - tp;
+			int a = hp + d;
+			int b = lp - d;
 
-			if (dt < min(abs(d1), abs(d2)) && ((d1 > 0 && d2 > 0) || (d1 < 0 && d2 < 0))) {
+			if (min(h, a) < max(l, b)) {
 				ans = false;
+				break;
 			}
 
-			pl = l;
-			ph = h;
-			pt = t;
+			lp = max(l, b);
+			hp = min(h, a);
+			tp = t;
 		}
 
 		if (ans) {
