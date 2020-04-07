4d2f7b700ef397d5d636e78bf847526e679a554f
Merisho <merishot@gmail.com>
2020-04-07 10:14:03 +0300

    D: solution

--- d_lunlun.cpp
+++ d_lunlun.cpp
@@ -1,45 +1,32 @@
-// UNSOLVED
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int k;
-
-long long gen(int i, long long n, int l) {
-	if (l == k) {
-		return n / 10;
-	}
-
-	long long a = 1e12, b = 1e12, c = 1e12;
-
-	if (i > 0) {
-		a = gen(i - 1, (n + i - 1) * 10, l + 1);
-	}
-
-	b = gen(i, (n + i) * 10, l + 1);
-
-	if (i < 9) {
-		c = gen(i + 1, (n + i + 1) * 10, l + 1);
-	}
-
-	return min(min(a, b), c);
-}
-
 int main() {
+	int k;
 	cin >> k;
 
-	if (k < 13) {
-		cout << k;
-		return 0;
+	queue<long long> q;
+	for (int i = 1; i < 10; ++i) {
+		q.push(i);
 	}
 
-	long long m = 1e12;
-	for (int i = 2; i < 10; ++i) {
-		long long n = gen(i, i * 10, 10 + 3 * (i - 1));
-		m = min(m, n);
+	for (int i = 0; i < k - 1; ++i) {
+		long long x = q.front();
+		q.pop();
+
+		int r = x % 10;
+		if (r > 0) {
+			q.push(x * 10 + r - 1);
+		}
+
+		q.push(x * 10 + r);
+
+		if (r < 9) {
+			q.push(x * 10 + r + 1);
+		}
 	}
 
-	cout << m;
+	cout << q.front();
 	
 	return 0;
 }
