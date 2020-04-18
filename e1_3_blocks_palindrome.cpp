56005708477c72f2ef562a439351c3eb1905457f
Merisho <merishot@gmail.com>
2020-04-18 10:32:29 +0300

    E1: solution

--- e1_3_blocks_palindrome.cpp
+++ e1_3_blocks_palindrome.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
 	int T;
 	cin >> T;
-
+ 
 	for (int t = 0; t < T; ++t) {
 		int n;
 		cin >> n;
-
+ 
+		int ans = 0;
 		vector<vector<int>> p(27, vector<int>(n + 1, 0));
 		for (int i = 0; i < n; ++i) {
 			int a;
@@ -16,41 +17,24 @@ int main() {
 			for (int j = 1; j < 27; ++j) {
 				p[j][i + 1] = p[j][i];
 			}
-
+ 
 			++p[a][i + 1];
+ 
+			ans = max(ans, p[a][i + 1]);
 		}
-
-		int m = 0;
-		for (int i = 1; i < 27; ++i) {
-			for (int j = 1; j < 27; ++j) {
-				int bf = -1;
-				int bl = -1;
-				int prev = 0;
-				for (int k = 1; k <= n; ++k) {
-					if (p[j][k] == 1 && bf < 0) {
-						bf = k;
-						bl = k;
-						prev = 1;
-					}
-
-					if (p[j][k] > prev) {
-						bl = k;
-						prev = p[j][k];
+ 
+		for (int a = 1; a < 27; ++a) {
+			for (int i = 1; i <= n && p[a][i] <= p[a][n] / 2; ++i) {
+				for (int b = 1; b < 27; ++b) {
+					for (int j = i + 1; j < n && p[a][n] - p[a][j] >= p[a][i]; ++j) {
+						ans = max(ans, p[a][i] * 2 + p[b][j] - p[b][i]);
 					}
 				}
-
-				if (bf == -1) {
-					continue;
-				}
-
-				int l = min(p[i][bf], p[i][n] - p[i][bl]) * 2 + p[j][bl];
-				m = max(m, l);
 			}
 		}
-
-		cout << m << endl;
+ 
+		cout << ans << endl;
 	}
 	
 	return 0;
 }
-	
