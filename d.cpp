5e9913a56c5135c175b4e0269556bffcdee61fce
Merisho <merishot@gmail.com>
2020-06-02 17:25:52 +0300

    D: remove unused function

--- d.cpp
+++ d.cpp
@@ -1,16 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool prime(int n) {
-	for (int i = 2; i <= sqrt(n); ++i) {
-		if (n % i == 0) {
-			return false;
-		}
-	}
-
-	return true;
-}
-
 int main() {
 	int T;
 	cin >> T;
