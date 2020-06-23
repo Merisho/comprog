45eb5e850efeaa86eb5cd3337d67547968163013
Merisho <merishot@gmail.com>
2020-06-23 16:32:11 +0300

    C: wrong solution

--- c.cpp
+++ c.cpp
@@ -1,50 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct cell {
-	int x;
-	int y;
-	int h;
-	int v;
-};
-
 int main() {
 	int n;
 	cin >> n;
 
-	int a = sqrt(n);
-	int b = n / a;
+	int a = (n + 1) / 2;
+	int m = a + (a % 2) + 2;
 
-	queue<cell> q;
-	q.push({0, 0, b - 1, a - 1});
-
-	set<pair<int, int>> s;
-	if (n % 2 == 1) {
-		q.push(cell{0, -1, 0, 0});
+	vector<pair<int, int>> ans;
+	int y = 0;
+	while (m > 0) {
+		for (int i = 0; i < m; ++i) {
+			ans.push_back({y + i, y});
+			ans.push_back({y + i, -y - 1});
+		}
+		++y;
+		m -= 2;
 	}
 
-	while (!q.empty()) {
-		cell c = q.front();
-		q.pop();
-		s.insert({c.x, c.y});
-
-		if (c.h > 0) {
-			q.push({c.x + 1, c.y, c.h - 1, c.v});
-		}
-
-		if (c.v > 0) {
-			q.push({c.x, c.y + 1, c.h, c.v - 1});
-		}
-
-		s.insert({c.x + 1, c.y});
-		s.insert({c.x - 1, c.y});
-		s.insert({c.x, c.y + 1});
-		s.insert({c.x, c.y -1});
-	}
-
-	cout << s.size() << endl;
-	for (pair<int, int> si : s) {
-		cout << si.first << " " << si.second << endl;
+	cout << ans.size() << endl;
+	for (auto a : ans) {
+		cout << a.first << " " << a.second << endl;
 	}
 	
 	return 0;
