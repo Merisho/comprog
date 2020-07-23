dd4eba1b3d6fed5bd9980dbca64095d461d3458a
Merisho <merishot@gmail.com>
2020-07-23 18:30:27 +0300

    C: solution

--- c.cpp
+++ c.cpp
@@ -10,38 +10,18 @@ int main() {
 		string s;
 		cin >> s;
 
-		int n = s.length();
-
-		vector<int> pm(n, 0);
-		vector<int> pp(n, 0);
-		
-		if (s[0] == '+') {
-			++pp[0];
-		} else {
-			++pm[0];
-		}
-
-		for (int i = 1; i < n; ++i) {
-			pm[i] = pm[i - 1];
-			pp[i] = pp[i - 1];
-
-			if (s[i] == '+') {
-				++pp[i];
-			} else {
-				++pm[i];
+		ll res = s.length();
+		int cur = 0;
+		int mn = 0;
+		for (int i = 0; i < s.length(); ++i) {
+			cur += s[i] == '+' ? 1 : -1;
+			if (cur < mn) {
+				mn = cur;
+				res += i + 1;
 			}
 		}
 
-		ll sum = n;
-		for (int i = 0; i < n; ++i) {
-			sum += i + 2 - pm[i] + pp[i];
-
-			if (i - pm[n - 1] + pp[n - 1] >= 0) {
-				break;
-			}
-		}
-
-		cout << sum << endl;
+		cout << res << endl;
 	}
 	
 	return 0;
