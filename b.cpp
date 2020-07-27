442377a0aea53e45db313b35df0b0b2409003020
Merisho <merishot@gmail.com>
2020-07-27 18:14:46 +0300

    B: solution

--- b.cpp
+++ b.cpp
@@ -1,6 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+vector<int> a;
+vector<int[4]> dp;
+
+int p(int i, int jj, int j) {
+	if (a[i - 1] < a[i] && jj >= j || a[i - 1] > a[i] && jj <= j || a[i - 1] == a[i] && jj != j) {
+		return 1;
+	}
+
+	return 0;
+}
+
 int main() {
 	int T;
 	cin >> T;
@@ -9,40 +20,28 @@ int main() {
 		int k;
 		cin >> k;
 
-		vector<int> a(k);
+		a = vector<int>(k);
 		for (int i = 0; i < k; ++i) {
 			cin >> a[i];
 		}
 
-		vector<int> b(k);
-		if (a[0] > a[1]) {
-			b[0] = 4;
-		} else {
-			b[0] = 1;
+		dp = vector<int[4]>(k);
+		for (int i = 0; i < 4; ++i) {
+			dp[0][i] = 0;
 		}
 
-		int ans = 0;
 		for (int i = 1; i < k; ++i) {
-			if (a[i] > a[i - 1]) {
-				b[i] = b[i - 1] + 1;
-			} else if (a[i] < a[i - 1]) {
-				b[i] = b[i - 1] - 1;
-			} else {
-				b[i] = b[i - 1];
-			}
-
-			if (b[i] > 4 || b[i] < 1) {
-				++ans;
-				if (i < k - 1) {
-					if (a[i] > a[i + 1]) {
-						b[i] = 4;
-					} else {
-						b[i] = 1;
-					}
-				} else {
-					b[i] = b[i - 1];
+			for (int j = 0; j < 4; ++j) {
+				dp[i][j] = dp[i - 1][0] + p(i, 0, j);
+				for (int jj = 1; jj < 4; ++jj) {
+					dp[i][j] = min(dp[i][j], dp[i - 1][jj] + p(i, jj, j));
 				}
 			}
+		}
+
+		int ans = 1e9;
+		for (int i = 0; i < 4; ++i) {
+			ans = min(ans, dp[k - 1][i]);
 		}
 
 		cout << "Case #" << t << ": " << ans << endl;
