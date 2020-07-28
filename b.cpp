eede0fd2f221aeb4babdfc73f2c6db26ce9a0082
Merisho <merishot@gmail.com>
2020-07-28 17:52:28 +0300

    B: solution

--- b.cpp
+++ b.cpp
@@ -1,40 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int pw(int a, int e) {
-	int r = 1;
-	for (int i = 0; i < e; ++i) {
-		r *= a;
-	}
-
-	return r;
-}
-
-int max_div(int n) {
-	int m = n;
-	int maxd = 0;
-	int maxd_count = 0;
+int min_f(int n) {
 	for (int i = 2; i * i <= n; ++i) {
-		int k = 0;
-		while (n % i == 0) {
-			n /= i;
-			++k;
-		}
-
-		if (pw(i, k) >= pw(maxd, maxd_count)) {
-			maxd = i;
-			maxd_count = k;
+		if (n % i == 0) {
+			return i;
 		}
 	}
 
-	int res = pw(maxd, maxd_count);
-	if (res == m) {
-		res /= maxd;
-	}
-
-	res = max(res, n);
-
-	return res;
+	return n;
 }
 
 int main() {
@@ -45,14 +19,9 @@ int main() {
 		int n;
 		cin >> n;
 
-		if (n % 2 == 0) {
-			int m = n / 2;
-			cout << m << " " << m << endl;
-			continue;
-		}
-
-		int d = max_div(n);
-		cout << d << " " << (n - d) << endl;
+		int f = min_f(n);
+		int k = n / f;
+		cout << k << " " << (n - k) << endl;
 	}
 	
 	return 0;
