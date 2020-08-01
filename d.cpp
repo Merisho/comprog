76643b2f9c3496df00968625a399187ba55dc7db
Merisho <merishot@gmail.com>
2020-08-01 10:49:44 +0300

    D: better solution

--- d.cpp
+++ d.cpp
@@ -6,46 +6,25 @@ int main() {
 	int n;
 	cin >> n;
 
-	vector<int> a(n + 1);
-	ll s = 0;
-	for (int i = 1; i <= n; ++i) {
+	vector<int> a(n);
+	for (int i = 0; i < n; ++i) {
 		cin >> a[i];
-		s += a[i];
+	}
+	
+	ll curr = 0;
+	int m = (n + 1) / 2;
+	for (int i = 0; i < m; ++i) {
+		curr += a[2 * i];
 	}
 
-	if (n == 1) {
-		cout << s;
-		return 0;
+	ll ans = curr;
+	for (int i = 0; i <= n - 2; ++i) {
+		curr -= a[(2 * i) % n];
+		curr += a[(2 * (i + m)) % n];
+		ans = max(ans, curr);
 	}
 
-	vector<int> b;
-	vector<ll> p(2 * (n + 2), 0);
-	int k = 1;
-	for (int i = 1; i <= n; i += 2) {
-		b.push_back(a[i]);
-		p[k] = p[k - 1] + a[i];
-		++k;
-	}
-
-	for (int i = 2; i < n; i += 2) {
-		b.push_back(a[i]);
-		p[k] = p[k - 1] + a[i];
-		++k;
-	}
-
-	int sz = b.size();
-	for (int i = 0; i < sz; ++i) {
-		b.push_back(b[i]);
-		p[k] = p[k - 1] + b[i];
-		++k;
-	}
-
-	ll mn = __LONG_LONG_MAX__;
-	for (int i = 0; i <= n; ++i) {
-		mn = min(mn, p[i + n / 2] - p[i]);
-	}
-
-	cout << (s - mn);
+	cout << ans;
 	
 	return 0;
 }
