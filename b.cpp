f462217f7853500c441ae49bc3246d07b2121c0a
Merisho <merishot@gmail.com>
2020-11-30 19:23:53 +0200

    B: solution

--- b.cpp
+++ b.cpp
@@ -10,40 +10,18 @@ int main() {
 		int n, k;
 		cin >> n >> k;
 
-		vector<vector<int>> p(101, vector<int>(n + 1, 0));
-		int mx = 0;
-		for (int i = 0; i < n; ++i) {
-			int c;
-			cin >> c;
-
-			for (int j = 1; j <= 100; ++j) {
-				p[j][i + 1] = p[j][i] + int(c == j);
-				mx = max(mx, p[j][n]);
-			}
-		}
-
-		if (k == 1) {
-			cout << (n - mx) << endl;
-			continue;
+		vector<int> c(n);
+		for (int& ci : c) {
+			cin >> ci;
 		}
 
 		int ans = 1e9;
 		for (int j = 1; j <= 100; ++j) {
-			if (p[j][n] - p[j][0] == 0) {
-				continue;
-			}
-
 			int d = 0;
-			int prev = -1;
-			for (int i = k; i <= n; ++i) {
-				int curr = k - (p[j][i] - p[j][i - k]);
-				
-				if (curr <= prev) {
+			for (int i = 0; i < n; ++i) {
+				if (c[i] != j) {
 					++d;
-					prev = -1;
-					i = min(n, i + k - 1);
-				} else {
-					prev = curr;
+					i += (k - 1);
 				}
 			}
 
