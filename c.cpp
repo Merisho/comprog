152d2fe4f4cb0e36bd919e9ee165dfa020b25086
Merisho <merishot@gmail.com>
2020-12-26 07:40:52 +0200

    C: solution

--- c.cpp
+++ c.cpp
@@ -2,6 +2,30 @@
 using namespace std;
 using ll = long long;
 
+int cycles;
+vector<bool> mark;
+vector<bool> pmark;
+vector<vector<int>> g;
+
+void dfs(int node) {
+    if (pmark[node]) {
+      return;
+    }
+    if (mark[node]) {
+      cycles++;
+      return;
+    }
+    mark[node] = true;
+
+    // Try all outgoing edges.
+    for (int neighbour : g[node]) {
+      dfs(neighbour);
+    }
+
+    pmark[node] = true;
+    mark[node] = false;
+  }
+
 int main() {
 	int T;
 	cin >> T;
@@ -10,22 +34,28 @@ int main() {
 		int n, m;
 		cin >> n >> m;
 
-		vector<pair<int, int>> r;
-		set<int> ox;
-		set<int> oy;
-		queue<int> q;
+		cycles = 0;
+		mark = vector<bool>(n + 1);
+		pmark = vector<bool>(n + 1);
+
+		g = vector<vector<int>>(n + 1);
+		int ans = m;
 		for (int i = 0; i < m; ++i) {
 			int x, y;
 			cin >> x >> y;
-
-			ox.insert(x);
-			oy.insert(y);
-			r.push_back({ x, y });
 			
 			if (x != y) {
-				q.push(i);
+				g[x].push_back(y);
+			} else {
+				--ans;
 			}
 		}
+
+		for (int i = 1; i <= n; ++i) {
+			dfs(i);
+		}
+
+		cout << (ans + cycles) << endl;
 	}
 	
 	return 0;
