b6cda36387b28441a36d4e5ac3f822c3fb4fff13
Merisho <merishot@gmail.com>
2021-01-21 18:19:30 +0200

    B: solution

--- b.cpp
+++ b.cpp
@@ -1,34 +1,14 @@
 #include <bits/stdc++.h>
+#include <numeric>
 using namespace std;
 using ll = long long;
 
-string repeat(string s, int n) {
-	string res = s;	
-	for (int i = 0; i < n; ++i) {
-		res += s;
-	}
-
-	return res;
+int gcd(int a, int b) {
+	return a == 0 ? b : gcd(b % a, a);
 }
 
-bool div(string s, string t) {
-	if (s.length() < t.length()) {
-		return false;
-	}
-
-	for (int i = 0; i < s.length(); ++i) {
-		for (int j = 0; j < t.length(); ++j) {
-			if (s[i] != t[j]) {
-				return false;
-			}
-
-			++i;
-		}
-
-		--i;
-	}
-
-	return true;
+int lcm(int a, int b) {
+	return a * b / gcd(a, b);
 }
 
 int main() {
@@ -39,16 +19,28 @@ int main() {
 		string s, t;
 		cin >> s >> t;
 
-		string ans = "-1";
-		for (int i = 0; i < 450; ++i) {
-			string ss = repeat(s, i);
-			if (div(ss, t)) {
-				ans = ss;
-				break;
-			}
+		int a = s.length();
+		int b = t.length();
+
+		int l = lcm(a, b);
+
+		string ss = s;
+		while (ss.length() != l) {
+			ss += s;
 		}
 
-		cout << ans << endl;
+		string tt = t;
+		while (tt.length() != l) {
+			tt += t;
+		}
+
+		if (ss == tt) {
+			cout << ss;
+		} else {
+			cout << -1;
+		}
+
+		cout << endl;
 	}
 	
 	return 0;
