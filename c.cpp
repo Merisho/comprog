5b4e14c7cb894a8b01c1607e679eebd6bc115e7c
Merisho <merishot@gmail.com>
2021-01-27 11:58:44 +0200

    C: solution

--- c.cpp
+++ c.cpp
@@ -2,90 +2,52 @@
 using namespace std;
 using ll = long long;
 
-int n;
-vector<int> a;
-vector<pair<int, int>> ans;
-vector<int> u;
-
-bool f(vector<vector<pair<int, int>>>& b, int i, int c = 0) {
-	if (c == n) {
-		return true;
-	}
-
-	for (pair<int, int> p : b[i]) {
-		if (u[p.first] || u[p.second]) {
-			continue;
-		}
-
-		ans.push_back(p);
-		
-		u[p.first] = 1;
-		u[p.second] = 1;
-		if (f(b, p.second, c + 1)) {
-			return true;
-		}
-
-		ans.pop_back();
-		u[p.first] = 0;
-		u[p.second] = 0;
-	}
-
-	return false;
-}
-
 int main() {
 	int T;
 	cin >> T;
 
 	for (int test_case = 1; test_case <= T; ++test_case) {
+		int n;
 		cin >> n;
 
-		a.resize(2 * n);
-		ans = vector<pair<int, int>>();
-		u = vector<int>(2 * n, 0);
+		vector<int> a(2 * n);
 		for (int& ai : a) {
 			cin >> ai;
 		}
 
 		sort(a.begin(), a.end());
 
-		vector<vector<pair<int, int>>> b(a.size());
-
+		int ss = -1;
+		vector<pair<int, int>> ans;
 		for (int i = 0; i < 2 * n - 1; ++i) {
-			a.push_back(a[2 * n - 1] + a[i]);
-			b.push_back({ { i, 2 * n - 1 } });
-		}
+			ans.clear();
+			multiset<int> s(a.begin(), a.end());
+			ss = a[2 * n - 1] + a[i];
+			int x = ss;
 
-		for (int i = 2 * n - 1; i > 1; --i) {
-			int l = 0;
-			int r = i - 1;
-			while (l < r) {
-				int s = a[l] + a[r];
-				if (s == a[i]) {
-					b[i].push_back({ l, r });
-					++l;
-					--r;
-				} else if (s > a[i]) {
-					--r;
+			for (int j = 0; j < n; ++j) {
+				int mx = *(s.rbegin());
+				s.erase(s.find(mx));
+				
+				if (s.count(x - mx) > 0) {
+					s.erase(s.find(x - mx));
+					ans.push_back({ mx, x - mx });
+					x = mx;
 				} else {
-					++l;
+					ss = -1;
+					break;
 				}
 			}
-		}
 
-		int ss = -1;
-		for (int i = a.size() - 1; i >= 2 * n; --i) {
-			if (f(b, i)) {
-				ss = a[i];
+			if (ss > -1) {
 				break;
 			}
 		}
 
 		if (ss > -1) {
-			cout << "YES" << endl;
-			cout << ss << endl;
-			for (pair<int, int> p : ans) {
-				cout << a[p.first] << " " << a[p.second] << endl;
+			cout << "YES" << endl << ss << endl;
+			for (int i = 0; i < ans.size(); ++i) {
+				cout << ans[i].first << " " << ans[i].second << endl;
 			}
 		} else {
 			cout << "NO" << endl;
