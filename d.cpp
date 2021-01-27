afc0fcd49e1788e7c3d9599ed53e36b023c8aec9
Merisho <merishot@gmail.com>
2021-01-27 13:50:07 +0200

    D: wrong solution

--- d.cpp
+++ d.cpp
@@ -10,37 +10,57 @@ int main() {
 		int n, m;
 		cin >> n >> m;
 
-		vector<pair<int, int>> a(n);
+		vector<int> a(n);
 		for (int i = 0; i < n; ++i) {
-			cin >> a[i].first;
+			cin >> a[i];
 		}
 
+		vector<int> b1;
+		vector<int> b2;
 		for (int i = 0; i < n; ++i) {
-			cin >> a[i].second;
-		}
+			int b;
+			cin >> b;
 
-		sort(a.begin(), a.end());
-
-		vector<pair<int, int>> t;
-		int s = 0;
-		int st = 1;
-		int sb = 0;
-		for (int i = n - 1; i > 0; --i) {
-			if (s >= m) {
-				if (s - t[st - 1].first + a[i].first >= m && t[st - 1].second > a[i].second) {
-					--sb;
-					s = s - t[st - 1].first + a[i].first;
-					t.pop_back();
-					t.push_back(a[i]);
-				}
+			if (b == 1) {
+				b1.push_back(a[i]);
 			} else {
-				s += a[i].first;
-				t.push_back(a[i]);
-				sb += a[i].second;
+				b2.push_back(a[i]);
 			}
 		}
 
-		cout << (sb == 0 ? -1 : sb) << endl;
+		sort(b1.begin(), b1.end(), greater<int>());
+		sort(b2.begin(), b2.end(), greater<int>());
+
+		int s = 0;
+		int ans = 2 * b2.size();
+		for (int i = 0; i < b2.size(); ++i) {
+			s += b2[i];
+		}
+
+		int l = 0;
+		int r = b2.size() - 1;
+
+		while (l < b1.size() && r >= 0) {
+			if (s < m) {
+				s += b1[l];
+				++l;
+				++ans;
+			} else if (s - b2[r] >= m) {
+				s -= b2[r];
+				--r;
+				ans -= 2;
+			} else if (s - b2[r] + b1[l] >= m) {
+				s -= b2[r];
+				--r;
+				s += b1[l];
+				++l;
+				--ans;
+			} else {
+				break;
+			}
+		}
+
+		cout << (s < m ? -1 : ans) << endl;
 	}
 	
 	return 0;
