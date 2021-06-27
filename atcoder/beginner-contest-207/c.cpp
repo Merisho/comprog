2f32737bfed86316a66f337847c220e3a99fb74a
Merisho <merishot@gmail.com>
2021-06-27 18:31:33 +0300

    C: solution

--- atcoder/beginner-contest-207/c.cpp
+++ atcoder/beginner-contest-207/c.cpp
@@ -6,40 +6,27 @@ int main() {
 	int N;
 	cin >> N;
 
-	vector<array<ll, 3>> a(N);
+	vector<pair<double, double>> a(N);
 	for (int i = 0; i < N; ++i) {
-		ll t, l, r;
+		double t, l, r;
 		cin >> t >> l >> r;
 
-		a[i] = {t, l, r};
+		if (t == 2) {
+			r -= 0.5;
+		} else if (t == 3) {
+			l += 0.5;
+		} else if (t == 4) {
+			l += 0.5;
+			r -= 0.5;
+		}
+
+		a[i] = {l, r};
 	}
 
 	int ans = 0;
 	for (int i = 0; i < N; ++i) {
 		for (int j = i + 1; j < N; ++j) {
-			ll mx = max(a[i][1], a[j][1]);
-			ll mn = min(a[i][2], a[j][2]);
-			ll t1 = a[i][0];
-			ll t2 = a[j][0];
-			if (mx == mn) {
-				if (t1 == 1) {
-					if (t2 == 2 && a[i][2] == a[j][1]) {
-						++ans;
-					} else if (t2 == 3 && a[i][1] == a[j][2]) {
-						++ans;
-					}
-				}
-
-				if (t2 == 1) {
-					if (t1 == 2 && a[i][1] == a[j][2]) {
-						++ans;
-					} else if (t1 == 3 && a[i][2] == a[j][1]) {
-						++ans;
-					}
-				}
-			}
-
-			if (mx < mn) {
+			if (max(a[i].first, a[j].first) <= min(a[i].second, a[j].second)) {
 				++ans;
 			}
 		}
