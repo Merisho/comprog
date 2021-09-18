a6f4d5e778b41d4ac86de931a49e032b040d1731
Merisho <merishot@gmail.com>
2021-09-18 14:15:15 +0300

    CF #729 Div. 2 - C: solution

--- codeforces/round-729-div-2/c.cpp
+++ codeforces/round-729-div-2/c.cpp
@@ -1,16 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+constexpr ll MOD = 1e9 + 7;
+
+ll gcd(ll a, ll b){
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) {
+	return a / gcd(a, b) * b;
+}
 
 int main() {
 	int T;
 	cin >> T;
 
 	for (int test_case = 1; test_case <= T; ++test_case) {
-		int p;
-		cin >> p;
+		ll n;
+		cin >> n;
 
-		cout << 2 << " " << (p - 1) << endl;
+		ll g = 1;
+		ll ans = 0;
+		for (ll i = 2; g <= n; ++i) {
+			ans += (n / g) % MOD;
+			g = lcm(g, i);
+		}
+
+		cout << ((ans + n) % MOD) << endl;
 	}
 	
 	return 0;
