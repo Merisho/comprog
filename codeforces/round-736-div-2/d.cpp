85355d7c0e62a9d7dcceafab94c13b8915304e67
Merisho <merishot@gmail.com>
2021-09-21 12:08:04 +0300

    CF #736 Div. 2 - D: solution

--- codeforces/round-736-div-2/d.cpp
+++ codeforces/round-736-div-2/d.cpp
@@ -6,6 +6,42 @@ ll gcd(ll a, ll b) {
 	return a == 0 ? b : gcd(b % a, a);
 }
 
+struct SegmentTree {
+	int left, right;
+	SegmentTree *leftChild, *rightChild;
+	ll gcd;
+
+	SegmentTree(int l, int r, vector<ll>& v) {
+		left = l;
+		right = r;
+		if (l == r) {
+			gcd = v[l];
+			return;
+		}
+
+		int mid = (l + r) / 2;
+		leftChild = new SegmentTree(l, mid, v);
+		rightChild = new SegmentTree(mid + 1, r, v);
+
+		gcd = __gcd(leftChild->gcd, rightChild->gcd);
+	}
+
+	ll query(int l, int r) {
+		if (r < left || l > right) {
+			return 0;
+		}
+
+		if (left >= l && right <= r) {
+			return gcd;
+		}
+		
+		ll lgcd = leftChild->query(l, r);
+		ll rgcd = rightChild->query(l, r);
+
+		return __gcd(lgcd, rgcd);
+	}
+};
+
 int main() {
 	int T;
 	cin >> T;
@@ -19,34 +55,20 @@ int main() {
 			cin >> ai;
 		}
 
-		int ans = 1;
-		ll pm = 0;
-		int k = 1;
-		for (int i = 1; i < n; ++i) {
-			ll p = a[i - 1];
-			ll ai = a[i];
-
-			ll m = abs(ai - p);
-			if (pm == 0) {
-				pm = m;
-			}
-
-			ll gm = gcd(m, pm);
-			if (gm > 1) {
-				++k;
-				pm = gm;
-			} else {
-				ans = max(ans, k);
-				k = 1;
-				pm = 0;
-
-				if (m > 1) {
-					--i;
-				}
-			}
+		vector<ll> d(n - 1);
+		for (int i = 0; i < n - 1; ++i) {
+			d[i] = abs(a[i] - a[i + 1]);
 		}
 
-		ans = max(ans, k);
+		SegmentTree st(0, n - 1, d);
+		int ans = 1;
+		int r = 0;
+		for(int i = 0; i < n - 1; i++) {
+			r = max(r, i);
+			while(r < n - 1 && st.query(i, r) > 1)
+				r++;
+			ans = max(ans, r - i + 1);
+		}		
 
 		cout << ans << endl;
 	}
