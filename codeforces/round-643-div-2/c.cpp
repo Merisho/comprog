2d3c312ef0407f602ae48e1a40c14a5857ddef75
Merisho <merishot@gmail.com>
2021-10-14 20:11:13 +0300

    CF #643 - C: solution

--- codeforces/round-643-div-2/c.cpp
+++ codeforces/round-643-div-2/c.cpp
@@ -2,23 +2,29 @@
 using namespace std;
 using ll = long long;
 
+const int n = 1e6 + 1;
+ll p[n];
+
 int main() {
 	ll a, b, c, d;
 	cin >> a >> b >> c >> d;
 
-	ll ans = 0;
-	ll y = c;
-	while (y >= b) {
-		for (ll x = a; x <= b; ++x) {
-			ll s = y + x;
-			if (s > d) {
-				ans += d - c + 1;
-			} else {
-				ans += s - c + 1;
-			}
-		}
+	for (int x = a; x <= b; ++x) {
+		++p[x + b];
+		--p[x + c + 1];
+	}
 
-		--y;
+	for (int i = 1; i < n; ++i) {
+		p[i] += p[i - 1];
+	}
+
+	for (int i = 1; i < n; ++i) {
+		p[i] += p[i - 1];
+	}
+
+	ll ans = 0;
+	for (int z = c; z <= d; ++z) {
+		ans += p[n - 1] - p[z];
 	}
 
 	cout << ans;
