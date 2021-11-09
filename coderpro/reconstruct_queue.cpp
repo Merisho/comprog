5404c67248c01a738773b1e6b11cac90c51bfbb8
Merisho <merishot@gmail.com>
2021-11-09 20:53:36 +0200

    Coderpro: reconstruct queue

--- coderpro/reconstruct_queue.cpp
+++ coderpro/reconstruct_queue.cpp
@@ -4,26 +4,23 @@ using ll = long long;
 
 int main() {
 	int n = 6;
-	pair<int, int> input[n] = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+	vector<pair<int, int>> input = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
 	
-	vector<int> a(n);
-	for (int i = 0; i < n; ++i) {
-		a[i] = i;
-	}
+	sort(input.begin(), input.end(), [](const auto& l, const auto& r) {
+		if (l.first == r.first) {
+			return l.second < r.second;
+		}
 
-	sort(a.begin(), a.end(), [&](int x, int y) {
-		return input[x].first >= input[y].first && input[x].second < input[y].second;
+		return l.first > r.first;
 	});
 
-	forward_list<pair<int, int>> l;
-	auto it = l.begin();
-	for (int i = 0; i < n; ++i) {
-		auto itt = it + 5;
-		l.insert_after(it + input[a[i]].second, input[a[i]]);
+	vector<pair<int, int>> res;
+	for (auto p : input) {
+		res.insert(res.begin() + p.second, p);
 	}
-
-	for (auto i = l.begin(); i != l.end(); ++i) {
-		cout << (*i).first << " " << (*i).second << endl;
+	
+	for (int i = 0; i < res.size(); ++i) {
+		cout << '{' << res[i].first << ' ' << res[i].second << "} ";
 	}
 
 	return 0;
